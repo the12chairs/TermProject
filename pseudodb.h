2d12f2edb46db84bc4c85f7217e73e2c96e1e71b
the12chairs <mbondarenko92@gmail.com>
2012-10-14 10:39:13 +0700

    creating a bicycle with file db

--- pseudodb.h
+++ pseudodb.h
@@ -1,10 +1,17 @@
 #ifndef PSEUDODB_H
+#include <QString>
 #define PSEUDODB_H
+
+/*
+ * Класс, реализующий работу с файловой "базой данных"
+ */
 
 class PseudoDB
 {
-public:
-    PseudoDB();
+public: PseudoDB(); // Конструктор по умолчанию
+public: ~PseudoDB(); // Деструктор
+public: bool createDB(QString dbname); //Создание файла базы данных
+public: bool isExists(QString path);
 };
 
 #endif // PSEUDODB_H
